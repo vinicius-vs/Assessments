bfad3b60c406b93d70fe122188e3e5bef56d4a3e
Matheus Frutuoso <matheusfx5@gmail.com>
2021-08-16 16:20:43 -0300

    Model Avaliation created Closed #8

---
+++ Assessments/Data/Model/Avaliation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Model
+{
+    public class Avaliation : Base
+    {
+        public int Grade { get; set; }
+    }
+}
