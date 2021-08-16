ba12b6fd7167b5a38b3855c545c1890737d5edaf
Matheus Frutuoso <matheusfx5@gmail.com>
2021-08-16 15:38:22 -0300

    Creating model for Tests

---
+++ Assessments/Data/Model/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Model
+{
+    public class Tests : Base
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Situation { get; set; }
+    }
+}
