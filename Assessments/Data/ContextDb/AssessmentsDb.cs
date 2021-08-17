f815f6f6d198b37689aa6f10e96661148ed5196e
Alex Junior <alexjuniorkuss@outlook.com>
2021-08-17 16:12:23 -0300

    Created Migration Phase 1#Part

--- Assessments/Data/ContextDb/AssessmentsDb.cs
+++ Assessments/Data/ContextDb/AssessmentsDb.cs
@@ -11,6 +11,7 @@ namespace Data.ContextDb
     public class AssessmentsDb : DbContext
     {
         public DbSet<Tests> Tests { get; set; }
+        public DbSet<Phase> Phase { get; set; }
         public AssessmentsDb() : base("Data Source=192.168.0.170;Initial Catalog=Assessments;Persist Security Info=True;User ID=Assessments;Password=projetox")
         {
                 
