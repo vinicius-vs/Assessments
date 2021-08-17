7be542a3e0aba3beb1f311a766151924f79d5963
vinicius <vinicius_stumpf@edu.univali.br>
2021-08-17 16:45:03 -0300

    criando migraite do avaliation part 1

--- Assessments/Data/ContextDb/AssessmentsDb.cs
+++ Assessments/Data/ContextDb/AssessmentsDb.cs
@@ -12,6 +12,7 @@ namespace Data.ContextDb
     {
         public DbSet<Tests> Tests { get; set; }
         public DbSet<Phase> Phase { get; set; }
+        public DbSet<Avaliation> Avaliation { get; set; }
         public AssessmentsDb() : base("Data Source=192.168.0.170;Initial Catalog=Assessments;Persist Security Info=True;User ID=Assessments;Password=projetox")
         {
                 
