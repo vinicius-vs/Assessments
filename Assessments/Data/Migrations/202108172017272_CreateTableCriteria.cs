2f03ce02ccb03a983261649d7844d58cdb7968c4
Matheus Frutuoso <matheusfx5@gmail.com>
2021-08-17 17:18:17 -0300

    Migration for Criteria created

---
+++ Assessments/Data/Migrations/202108172017272_CreateTableCriteria.cs
@@ -0,0 +1,27 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTableCriteria : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Criteria",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Criteria");
+        }
+    }
+}
